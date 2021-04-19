c277df9bb96672f186cf3e4cf3aae206592822fe
Pavan-kumar261 <pavan.kumar@nineleaps.com>
2021-04-19 17:21:56 +0530

    Revert "Modified server file"
    This reverts commit 16ed485745c9f905599807ef54f25d72b0ee1d63.

--- server/index.js
+++ server/index.js
@@ -4,11 +4,7 @@ import { handleRender } from "./initialState";
 const PORT = process.env.PORT || 3006;
 const app = express();
 
-// app.use(express.static("./build"));
-const publicDirectoryPath = path.join(__dirname, "../public");
-
-app.use(express.static(publicDirectoryPath));
-
+app.use(express.static("./build"));
 app.use(handleRender);
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
